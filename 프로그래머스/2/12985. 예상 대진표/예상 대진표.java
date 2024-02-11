bf122acd9ed70a0d10d4d1bb7ec40426b41a85da
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-02-11 17:07:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -3,31 +3,11 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        
-        int A = a;
-        int B = b;
-        
-        if(a>b){
-            A = b;
-            B = a;
-        } 
-
-        while(A!=B){
+        while(a!=b){
             answer++;
-            
-            if(A%2==0){
-                A /= 2;
-            }else{
-                A = (A+1)/2;
-            }
-            
-            if(B%2==0){
-                B /= 2;
-            }else{
-                B = (B+1)/2;
-            }
+            a = a/2 + a%2;
+            b = b/2 + b%2;
         }
-        
         return answer;
     }
 }
