ea0222dd36ac7fbe70194b05d8be49b06dbe59b6
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-02-11 16:35:25 +0900

    [level 2] Title: 구명보트, Time: 9.06 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,19 +4,19 @@ class Solution {
     public int solution(int[] people, int limit) {
 		int answer = 0;
 		Arrays.sort(people);
-		int min_index = 0; 
-		int max_index = people.length - 1; 
-		while(min_index <= max_index) {
-			if(people[max_index] + people[min_index] <= limit) {
-				answer++;
-				max_index--;
-				min_index++;
-			}
-			else {
-				answer++;
-				max_index--;
-			}
-		}
+		int lidx = 0;
+        int ridx = people.length-1;
+        
+        while(lidx<=ridx){
+            if(people[lidx] + people[ridx] <= limit){
+                answer++;
+                lidx++;
+                ridx--;
+            }else{
+                answer++;
+                ridx--;
+            }
+        }
         
         return answer;
     }
