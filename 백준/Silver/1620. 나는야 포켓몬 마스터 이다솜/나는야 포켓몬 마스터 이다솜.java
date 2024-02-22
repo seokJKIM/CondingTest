01abbce59ec14cef5a4bcf2d4f53e935cd0ce3d7
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-02-22 09:34:37 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 496 ms, Memory: 64492 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -8,31 +9,33 @@ public class Main {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer tokens;
     static StringBuilder output = new StringBuilder();
-    static int N,M;
-    static HashMap<String,String> map = new HashMap<>();
-    static HashMap<String,String> map2 = new HashMap<>();
+    static int N, M;
+    static HashMap<String, String> map1 = new HashMap<>();
+    static HashMap<String, String> map2 = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         tokens = new StringTokenizer(input.readLine());
 
         N = Integer.parseInt(tokens.nextToken());
-
         M = Integer.parseInt(tokens.nextToken());
 
-        for(int i=1; i<=N; i++){
+        for(int i=1; i<N+1; i++){
             String st = input.readLine();
-            map.put(String.valueOf(i),st);
-            map2.put(st,String.valueOf(i));
+
+            map1.put(st, String.valueOf(i));
+            map2.put(String.valueOf(i), st);
         }
 
         for(int i=0; i<M; i++){
             String st = input.readLine();
 
-            String ans = map.get(st);
-            if(ans == null) ans = String.valueOf(map2.get(st));
+            String ans = map1.get(st);
+
+            if(ans == null) ans = map2.get(String.valueOf(st));
 
             output.append(ans).append("\n");
         }
+
         System.out.println(output);
     }
 }
