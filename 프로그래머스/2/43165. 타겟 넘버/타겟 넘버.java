627bd908c6dc53550b0040ec0375564159489ed7
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-03-05 17:10:40 +0900

    [level 2] Title: 타겟 넘버, Time: 0.35 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,37 +1,23 @@
 class Solution {
     
-    static int[] calculate = {1,-1};
-    
     static int answer;
     
     public int solution(int[] numbers, int target) {
         answer = 0;
         
-        int[] choosed = new int[numbers.length];
-        
-        for(int i=0; i<numbers.length; i++){
-            choosed[i] = numbers[i];
-        }
-        dfs(0, numbers, target, choosed);        
+        dfs(0, numbers, target, 0);        
         
         return answer;
     }
     
-    static void dfs(int lv, int[] numbers, int target, int[] choosed){
+    static void dfs(int lv, int[] numbers, int target, int sum){
         if(lv == numbers.length){
-            int sum = 0;
-            for(int i=0; i<choosed.length; i++){
-                sum += choosed[i];
-            }            
-            
             if(sum == target) answer++;
-            
             return;
         }
         
-        for(int i=0; i<2; i++){
-            choosed[lv] = choosed[lv]*calculate[i];
-            dfs(lv+1, numbers, target, choosed);
-        }
+        
+        dfs(lv+1, numbers, target, sum + numbers[lv]);
+        dfs(lv+1, numbers, target, sum + numbers[lv]*(-1));
     }
 }
