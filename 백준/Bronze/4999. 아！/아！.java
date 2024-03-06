e26b93023503c0d8169e2c3b9bc9afe085c6f787
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-03-06 23:38:36 +0900

    [Bronze V] Title: 아!, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(input.readLine().length()>=input.readLine().length()?"go":"no");
+    }
+}
