9732b9bd83905afdf01bd946122718f16bcc1d88
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-03-21 21:22:32 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 24656 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.PriorityQueue;
 
 class Main {
@@ -13,15 +12,15 @@ class Main {
 
         N = Integer.parseInt(input.readLine());
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int i=0; i<N; i++){
             x = Integer.parseInt(input.readLine());
 
             if(x == 0){
                 if(pq.size() == 0) output.append(0).append("\n");
-                else output.append(pq.poll()).append("\n");
-            }else pq.add(x);
+                else output.append(pq.poll()*(-1)).append("\n");
+            }else pq.add(x*(-1));
         }
 
         System.out.println(output);
