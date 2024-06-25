1a403d4ee5dc617ebd7c2fe9f733e17945b2e9c8
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-06-25 20:46:30 +0900

    [level 2] Title: 올바른 괄호, Time: 18.51 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,26 +1,27 @@
 import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
-        Stack<Character> stack = new Stack<>();
-
+        
+        Deque<Character> deque = new ArrayDeque<>();
+        
         for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
+            Character c = s.charAt(i);
             
-            if(c == '(') stack.push(c);
-            else{
-                if(stack.isEmpty()){
-                    answer = false;
-                    break;
-                }else{
-                    if(stack.peek() == '(') stack.pop();
-                    else stack.push(')');
-                }
+            if(deque.size() == 0){
+                deque.addFirst(c);
+                continue;
+            }
+            
+            if(deque.peekFirst() == '(' && c == ')'){
+                deque.pollFirst();
+            }else{
+                deque.addFirst(c);
             }
         }
-
-        if(!stack.isEmpty()) answer = false;
+        
+        if(deque.size() != 0) answer = false;
         
         return answer;
     }
