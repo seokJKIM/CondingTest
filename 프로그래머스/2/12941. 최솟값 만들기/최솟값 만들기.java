a2a38dea57a813106f3df64d0793f68f8ecb4756
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-06-25 20:50:47 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.93 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,7 +1,9 @@
 import java.util.*;
+
 class Solution
 {
-    public int solution(int []A, int []B){
+    public int solution(int []A, int []B)
+    {
         int answer = 0;
 
         Arrays.sort(A);
