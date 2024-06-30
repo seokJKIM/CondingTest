9dd8f2b7f0e0a27587fcc9effeb4b35f71079ba2
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-06-30 21:26:42 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
+
         for(int i=1; i<=yellow/2+1; i++){
             if(yellow%i == 0){
-                int row = i;
-                int col = yellow/row;
-            
-                if(row*2+(col+2)*2 == brown){
-                    answer[0] = col+2;
-                    answer[1] = row+2;
+                int r = i;
+                int c = yellow/i;
+
+                if((r*2+c*2+4) == brown){
+                    answer[0] = c+2;
+                    answer[1] = r+2;
                     break;
                 }
             }
-            
         }
-        
         return answer;
     }
 }
