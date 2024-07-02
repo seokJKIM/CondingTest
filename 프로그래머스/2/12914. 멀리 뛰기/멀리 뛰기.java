91918bd0f8842cd95d45b633b14bfecf8cfe12f3
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-07-02 22:36:06 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,8 +1,10 @@
 class Solution {
     public long solution(int n) {
-        int[] dp = new int[2001];
+        int[] dp = new int[n+2];
+        
         dp[1] = 1;
         dp[2] = 2;
+        
         for(int i=3; i<dp.length; i++){
             dp[i] = (dp[i-1]+dp[i-2]) % 1234567;
         }
