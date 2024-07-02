2c8871ffd5bec8f39872f8083a4af09c4ae0e335
SJkim <147961068+seokJKIM@users.noreply.github.com>
2024-07-02 22:30:11 +0900

    [level 2] Title: 구명보트, Time: 9.39 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,23 +1,23 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
-        Arrays.sort(people);
+		int answer = 0;
+		Arrays.sort(people);
+		int lidx = 0;
+        int ridx = people.length-1;
         
-        int left = 0;
-        int right = people.length-1;
-        
-        while(left <= right){
-            if(people[left] + people[right] <= limit){
+        while(lidx<=ridx){
+            if(people[lidx] + people[ridx] <= limit){
                 answer++;
-                left++;
-                right--;
+                lidx++;
+                ridx--;
             }else{
                 answer++;
-                right--;
+                ridx--;
             }
         }
+        
         return answer;
     }
 }
